5360d5d976e818271ec9cd5364d4fb2a6078eb87
RuggedDrip5 <afinoshka@gmail.com>
2025-05-05 18:48:33 +0300

    Create CarRepository and implementation

---
+++ homework017/src/main/java/homework09/CarRepository.java
@@ -0,0 +1,9 @@
+package homework09;
+
+import java.util.List;
+
+public interface CarRepository {
+    List<Car> findAll();
+    void save(Car car);
+    Car findByBrandAndModel(String brand, String model);
+}
